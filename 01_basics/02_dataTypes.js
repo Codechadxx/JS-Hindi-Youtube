1d1b9e55813dc0cd697d1e9577342a66c07ec68d
Codechadxx <154961524+Codechadxx@users.noreply.github.com>
2024-07-03 06:35:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version
+
+// alert( 3 + 3) // wen are using nodejs , not browser
+
+console.log(3 
+    +
+    3)  // code readability should be high
+
+console.log("Rachit");
+
+let name = "Rachit"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+//bigint  
+// string => ""
+// boolean => true/false
+//null => standalone value => object hai
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
