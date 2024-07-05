5acee853638ba6b08e1fd4d3c5174148a851f374
Codechadxx <154961524+Codechadxx@users.noreply.github.com>
2024-07-05 15:34:59 +0000

    comparision of datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// the comparison convert null to a 
+// number , treating it a 0
+// that's why (3) null >= 0 is true and
+// (1) null > 0 is false.
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// if u do comparision of undefined with 
+// anything it will give false 
+
+// === => it compares both data type and number 
+
+console.log("2" === 2);
+
+
+
