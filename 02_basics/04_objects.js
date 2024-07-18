6d9bb2f0b578c6029fb995f9fe18bd9231d15754
Codechadxx <154961524+Codechadxx@users.noreply.github.com>
2024-07-18 17:36:09 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -55,3 +55,30 @@ console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLogged'));
+
+const course = {
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursenaem":"js in hindi",
+//     "price": "free"
+// }
+
+// line 72-76 is json code 
+
+[
+    {},
+    {},
+    {}
+]
